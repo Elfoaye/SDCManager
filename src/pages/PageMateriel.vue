43780341d5e2368929654b7bd99b2b1dcec3df1d
Samuel Bibas <thephoenixof05@gmail.com>
2025-05-21 09:20:11 +0200

    Ajout de la possibilitée de trier le materiel par colomne

--- src/pages/PageMateriel.vue
+++ src/pages/PageMateriel.vue
@@ -1,26 +1,68 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
-const list_content = ref(
-    [{nom:"Stairville Retro Flat Par 18x10W RGBWA UV",  type:"Lumière", disponible:4, total:12, contribution:9, valeur:269},
-    {nom:"JBL EON710",  type:"Diff", disponible:2, total:2, contribution:15, valeur:535}]
-)
+const list_content = [
+    {nom:"Stairville Retro Flat Par 18x10W RGBWA UV",  type:"Lumière", disponible:8, total:12, contribution:9, valeur:269},
+    {nom:"JBL EON710",  type:"Diff", disponible:2, total:2, contribution:15, valeur:535}, 
+    {nom:"Shure SM58 LC ",  type:"Micros", disponible:6, total:16, contribution:4, valeur:109}];
+
+const columns = [
+  { label: 'Nom', key: 'nom' },
+  { label: 'Catégorie', key: 'type' },
+  { label: 'Disponible', key: 'disponible' },
+  { label: 'Total', key: 'total' },
+  { label: 'Prix/Journée', key: 'contribution' },
+  { label: 'Valeur', key: 'valeur' }
+]
+
+const sort_property = ref(null);
+const sort_asc = ref(true);
+
+function set_sort(property) {
+    if(sort_property.value == property) {
+        sort_asc.value = !sort_asc.value;
+        return;
+    }
+    sort_asc.value = true;
+    sort_property.value = property;
+}
+
+const sorted_content = computed(() => {
+    if(!sort_property.value) return list_content;
+
+    return [...list_content].sort((a, b) => {
+        console.log(sort_property.value);
+        const valA = a[sort_property.value] ;
+        const valB = b[sort_property.value];
+    
+        if(typeof valA === 'number' && typeof valB === 'number') {
+            return sort_asc.value ? valA - valB : valB - valA;
+        } else {
+            return sort_asc.value ? String(valA).localeCompare(String(valB)) :
+                                    String(valB).localeCompare(String(valA));
+        }
+    });
+});
 </script>
 
 <template>
     <div class="content">
-        <h1>Voici tout le matériel</h1>
+        <div class="search">
+            <input type="text" placeholder="Chercher par nom..."/>
+            <button>Filtrer</button>
+        </div>
 
         <ul>
             <li class = "head">
-                <p>Nom</p>
-                <p>Catégorie</p>
-                <p>Disponible</p>
-                <p>Total</p>
-                <p>Prix/Journée</p>
-                <p>Valeur</p>
+                <button 
+                    v-for="col in columns"
+                    :key="col.key"
+                    @click="set_sort(col.key)"
+                >
+                {{ col.label }} <span v-if="sort_property === col.key">{{ sort_asc ? '▲' : '▼' }}</span>
+            </button>
             </li>
-            <li v-for="item in list_content">
+            <li v-for="item in sorted_content">
                 <p>{{ item.nom }}</p>
                 <p>{{ item.type }}</p>
                 <p>{{ item.disponible }}</p>
@@ -39,6 +81,20 @@ const list_content = ref(
     flex-direction: column;
     max-width: 60rem;
     padding: 2rem;
+}
+
+.search {
+    margin-bottom: 2rem;
+}
+
+input {
+    padding: 0.5rem;
+}
+
+.search button {
+    height: 100%;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
 }
 
 ul {
@@ -70,6 +126,21 @@ li p {
 
 li.head {
     border-bottom: 1px solid var(--border-accent);
-    font-weight: 500;
+}
+
+.head button {
+    display: inline-flex;
+    border: none;
+    cursor: pointer;
+    padding-left: 0;
+    padding-bottom: 0.5rem;
+    background-color: var(--background);
+    text-align: start;
+    font-weight: 600;
+}
+
+.head button span {
+  display: inline;
+  margin-left: 0.25rem;
 }
 </style>
