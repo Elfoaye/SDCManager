657721b2059f2bbf1237edb079fc0bdb172dc0fc
Samuel Bibas <thephoenixof05@gmail.com>
2025-06-05 16:27:27 +0200

    Ajout des champs de paramêtres dans le front end

--- src/pages/PageAdmin.vue
+++ src/pages/PageAdmin.vue
@@ -3,9 +3,42 @@
 </script>
 
 <template>
-    <h1>Page Admin</h1>
+    <div class="content">
+        <h1>Page Admin</h1>
+        <div class="params">
+            <section>
+                <h2>Materiel</h2>
+                <div class="formulas">
+                    <h3>Formules</h3>
+                    <label>Contribution de base (multiple la valeur) :
+                        <input type="number" />
+                    </label>
+                    <p>(X % du la valeur)</p>
+                    <label>Contribution suivante (multiple la contribution de base) :
+                        <input type="number" />
+                    </label>
+                    <p>(X % de la contribution de base)</p>
+                </div>
+                <div class="Types">
+                    <button>Ajouter</button>
+                    <ul>
+                        <li>Accessoires</li>
+                    </ul>
+                </div>
+            </section>
+            <section>
+                <h2>Admin</h2>
+                <div class="password">
+                    <button>Changer le mot de passe</button>
+                </div>
+            </section>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-
+.content {
+    display: flex;
+    flex-direction: column;
+}
 </style>
--- src/pages/PageParams.vue
+++ src/pages/PageParams.vue
@@ -3,9 +3,20 @@
 </script>
 
 <template>
-    <h1>Paramètres</h1>
+    <div class="content">
+        <h1>Paramètres</h1>
+        <div class="params">
+            <section>
+                <h2>Visuels</h2>
+                <label>Mode sombre : <input type="checkbox"></label>
+            </section>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-
+.content {
+    display: flex;
+    flex-direction: column;
+}
 </style>
