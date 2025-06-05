11622508ca7efeaebd12ef77a00baf2133476406
Samuel Bibas <thephoenixof05@gmail.com>
2025-06-05 11:35:45 +0200

    Ajout de la page d'authentification dans le front end

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <script setup>
 import { ref } from 'vue';
+import { invoke } from '@tauri-apps/api/core';
 
 import NavBar from './components/NavBar.vue';
 import HeaderBar from './components/HeaderBar.vue';
 import PageAccueil from './pages/PageAccueil.vue'
 import PageMateriel from './pages/PageMateriel.vue';
+import PageAuth from './pages/PageAuth.vue';
 
 const currentPage = ref(null);
 const lastPage = ref(null);
+const redirect = ref(null);
 
-function setPage(value) {
+async function setPage(value) {
     if (currentPage.value == value) return;
+
+    if(value === 'modif') {
+        const admin = await invoke('is_admin');
+        if(!admin) {
+            lastPage.value = currentPage.value;
+            redirect.value = value;
+            currentPage.value = 'auth';
+            return;
+        }
+    }
 
     lastPage.value = currentPage.value;
     currentPage.value = value;
@@ -29,8 +42,15 @@ function setPage(value) {
             <NavBar :setPage="setPage"/>
 
             <div class="content">
+                <PageAuth 
+                    v-if="currentPage === 'auth'" 
+                    :redirect="redirect"
+                    :setPage="setPage"
+                    @cancel="setPage(lastPage)"
+                />
+
                 <PageMateriel 
-                    v-if="currentPage === 'consult' || currentPage === 'modif'" 
+                    v-else-if="currentPage === 'consult' || currentPage === 'modif'" 
                     :modif="currentPage === 'modif'"
                 />
                 <PageAccueil v-else/>
---
+++ src/pages/PageAuth.vue
@@ -0,0 +1,107 @@
+<script setup>
+import { invoke } from '@tauri-apps/api/core';
+import { ref } from 'vue';
+
+const props = defineProps(['redirect','setPage']);
+const emit = defineEmits(['cancel'])
+
+const password = ref('');
+const password_error = ref('');
+
+async function confirmPassword() {
+    try {
+        await invoke('log_in_admin', { password: password.value});
+        if(invoke('is_admin')) {
+            password_error.value = null;
+            props.setPage(props.redirect);
+        } else {
+            password_error.value = "Authentification échouée, veuillez réessayer"
+        }
+    } catch(err) {
+        password_error.value = err;
+    }
+
+}
+</script>
+
+<template>
+    <div class="content">
+        <h1>Minute papillon !</h1>
+        <h2>Cette section requière des droits admin</h2>
+        <div class="submit">
+            <div class="field">
+                <input v-bind="password" placeholder="Mot de passe..."/>
+                <p v-if="password_error" class="error">{{ password_error }}</p>
+            </div>
+            <button class="confirm" @click="confirmPassword">Confirmer</button>
+            <button class="cancel" @click="emit('cancel')">Annuler</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.content {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.submit {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+    width: 100%;
+    max-width: 30rem;
+}
+
+.submit .field {
+    grid-column: span 2;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+}
+
+input {
+    align-self: center;
+    width: 100%;
+    max-width: 20rem;
+    background-color: var(--background-alt);
+    color: var(--text);
+    border: 1px solid var(--border);
+    border-radius: 0.3rem;
+    padding: 0.5rem;
+}
+
+button {
+    justify-self: end;
+    width: 100%;
+    height: 100%;
+    padding: 1rem;
+    margin-right: 1rem;
+    max-width: 8rem;
+    color: var(--text);
+    background-color: var(--accent);
+    border: 1px solid var(--border);
+    border-radius: 0.5rem;
+
+    transition: all 0.2s;
+}
+
+button:hover {
+    background-color: var(--surface-selected);
+    cursor: pointer;
+    
+    transition: all 0.2s;
+}
+
+button.cancel {
+    justify-self: start;
+    background-color: var(--disabled);
+}
+
+.error {
+    color: var(--error);
+}
+</style>
