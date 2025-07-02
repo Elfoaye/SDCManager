4fe5ae534fb2efa08b088b54d994edb41925318d
Samuel Bibas <thephoenixof05@gmail.com>
2025-07-02 15:45:07 +0200

    Ajout de fullCallendar pour le planning

--- src/pages/PageCalendrier.vue
+++ src/pages/PageCalendrier.vue
@@ -1,16 +1,55 @@
 <script setup>
-function handleDayClick(date) {
-    console.log('Afficher détails du jour :', date);
-}
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import frLocale from '@fullcalendar/core/locales/fr';
+import interactionPlugin from '@fullcalendar/interaction';
+
+const calendarOptions = {
+    plugins: [dayGridPlugin, interactionPlugin],
+    initialView: 'dayGridMonth',
+    locale: frLocale,
+    events: [
+        {
+            title: 'Fête de la musique',
+            start: '2025-07-02',
+            end: '2025-07-06',
+            id: 20250601
+        },
+        {
+            title: 'Verticalété',
+            start: '2025-06-28',
+            end: '2025-07-04',
+            id: 20250602
+        },
+        {
+            title: 'Pôtes de marmots',
+            start: '2025-07-08',
+            end: '2025-07-09',
+            id: 20250701
+        }
+    ],
+    eventClick(info) {
+        handleEventClick(info.event);
+    },
+    dateClick(info) {
+        handleDayClick(info.dateStr);
+    }
+};
 
 function handleEventClick(event) {
-    console.log('Rediriger vers le devis :', event);
+    console.log('Rediriger vers le devis :', event.id);
     
+}
+
+function handleDayClick(date) {
+    console.log('Afficher détails du jour :', date);
 }
 </script>
 
 <template>
-    
+    <div class="cal-container">
+        <FullCalendar :options="calendarOptions" />
+    </div>
 </template>
 
 <style>
